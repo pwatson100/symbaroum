d42a8746a48bb9bcf04c91b127bf136466a2cd8d
bithir <bithir@gmail.com>
2021-01-01 02:13:17 +0000

    Bug fix + Dice improvements
    Bug fix for Monster not registering experience requirements after initial login
    Configurable options:
    - Rare crit
    - No guaranteed success/failure flag
    - Crit/fail for non-combat elements
    - Damage modifiers in roll dialogue
    - Allowance of favour/disfavour in roll dialogue
    - Automated Support for quality Precise & deep impact
    - Hide fields when not in use (no target selected, etc. etc)

--- script/common/dialog.js
+++ script/common/dialog.js
@@ -1,32 +1,95 @@
 import { rollAttribute } from './roll.js';
 
+let roll_defaults = {};
+
 export async function prepareRollAttribute(character, attribute, armor, weapon) {
-    const html = await renderTemplate('systems/symbaroum/template/chat/dialog.html', {});
-    let dialog = new Dialog({
-        title: attribute.name,
-        content: html,
-        buttons: {
-            roll: {
-                icon: '<i class="fas fa-check"></i>',
-                label: game.i18n.localize('BUTTON.ROLL'),
-                callback: async (html) => {
-                    const modifierName = html.find('#modifier')[0].value;
-                    const bonus = html.find('#bonus')[0].value;
-                    const modifier = getTargetAttribute(modifierName, bonus);
-                    await rollAttribute(character, attribute, modifier, armor, weapon);
-                },
-            },
-            cancel: {
-                icon: '<i class="fas fa-times"></i>',
-                label: game.i18n.localize('BUTTON.CANCEL'),
-                callback: () => {},
-            },
-        },
-        default: 'roll',
-        close: () => {},
-    });
-    dialog.render(true);
+	let attri_defaults = getRollDefaults(attribute.name,armor != null, weapon != null);
+
+  const html = await renderTemplate('systems/symbaroum/template/chat/dialog.html', {
+    "hasTarget": game.user.targets.values().next().value !== undefined,
+    "isWeaponRoll" : weapon !== null,
+    "isArmorRoll" : armor !== null,
+    "choices": { "0": game.i18n.localize("DIALOG.FAVOUR_NORMAL"), "-1":game.i18n.localize("DIALOG.FAVOUR_DISFAVOUR"), "1":game.i18n.localize("DIALOG.FAVOUR_FAVOUR")},
+    "groupName":"favour",
+    "roll_defaults": attri_defaults	
+  });
+  
+  let dialog = new Dialog({
+    title: attribute.name,
+    content: html,
+    buttons: {
+      roll: {
+        icon: '<i class="fas fa-check"></i>',
+        label: game.i18n.localize('BUTTON.ROLL'),
+        callback: async (html) => {
+          let dummyMod = "custom";			
+          if( html.find("#modifier").length > 0) {
+            dummyMod = html.find("#modifier")[0].value;											
+          }
+          attri_defaults.targetAttribute = dummyMod;
+          const modifierName = dummyMod;
+
+          let hasAdvantage = html.find("#advantage").length > 0;
+          if( hasAdvantage ) {
+            // Note that this turns into disadvantage for Defense rolls
+            hasAdvantage = html.find("#advantage")[0].checked;
+          }					
+          attri_defaults.advantage = hasAdvantage ? "checked":"";
+          const advantage = hasAdvantage; 
+                             
+          let hasDamModifier = html.find("#dammodifier").length > 0;
+          let damModifier = "";
+          if(hasDamModifier) {
+            damModifier = html.find("#dammodifier")[0].value;
+          }
+          attri_defaults.additionalModifier = damModifier;
+                    
+          let favours = html.find("input[name='favour']");
+          let fvalue = 0;
+          for ( let f of favours) {						
+            if( f.checked ) fvalue = f.value;
+          }
+          attri_defaults.selectedFavour = ""+fvalue;			
+          const favour = fvalue;
+          
+          const bonus = html.find("#bonus")[0].value;   
+          attri_defaults.bonus = bonus;              
+          
+          const modifier = getTargetAttribute(modifierName, bonus);                   
+
+          await rollAttribute(character, attribute, favour, modifier, armor, weapon, advantage, damModifier);
+          },
+      },
+      cancel: {
+          icon: '<i class="fas fa-times"></i>',
+          label: game.i18n.localize('BUTTON.CANCEL'),
+          callback: () => {},
+      },
+    },
+    default: 'roll',
+    close: () => {},
+  });
+  dialog.render(true);
 }
+
+function getRollDefaults(attributeName, isArmor, isWeapon) {
+	if( roll_defaults[attributeName+":"+isArmor+":"+isWeapon] === undefined )
+	{
+		roll_defaults[attributeName+":"+isArmor+":"+isWeapon] = createDefaults();
+	}
+	return roll_defaults[attributeName+":"+isArmor+":"+isWeapon];
+}
+
+function createDefaults() {
+	return {
+		targetAttribute: game.i18n.localize('ATTRIBUTE.CUSTOM'),
+		additionalModifier: "",
+		selectedFavour: "0",
+		bonus: 0,
+		advantage: "",
+	};
+}
+
 
 function getTargetAttribute(attributeName, bonus) {
     const target = game.user.targets.values().next().value;
