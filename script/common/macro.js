7c94bbf8ae87be629fb0471f9a3fd0213009f59b
Peter Norell <bithir@gmail.com>
2025-06-26 17:09:38 +0100

    Alternate Damage Macro #652 #698

--- script/common/macro.js
+++ script/common/macro.js
@@ -553,9 +553,73 @@ export class SymbaroumMacros {
     ui.notifications.info(`Actor with name ${npcname} is now a ${update.type}.`);
 	}
     /**********************************************
-     * Macro: 
+     * Macro: dealAlternativeDamage
      */
+		async dealAlternativeDamage() {
+			if (canvas.tokens.controlled.length === 0)
+				return ui.notifications.error("Please select a token first");
 
+			new Dialog({
+				title: `Alternate Damage`,
+				content: `
+					<form>
+					<div style="display:flex">
+					<label style="min-width:15em" for="damagetype">Damage type:</label>
+					<select id="vision-type" style="min-width:10em" name="damagetype">
+					<option value="accurate">${game.i18n.localize("ATTRIBUTE.ACCURATE")}</option>
+					<option value="cunning">${game.i18n.localize("ATTRIBUTE.CUNNING")}</option>
+					<option value="discreet">${game.i18n.localize("ATTRIBUTE.DISCREET")}</option>
+					<option value="persuasive">${game.i18n.localize("ATTRIBUTE.PERSUASIVE")}</option>
+					<option value="quick">${game.i18n.localize("ATTRIBUTE.QUICK")}</option>
+					<option value="resolute">${game.i18n.localize("ATTRIBUTE.RESOLUTE")}</option>
+					<option value="strong">${game.i18n.localize("ATTRIBUTE.STRONG")}</option>
+					<option value="vigilant">${game.i18n.localize("ATTRIBUTE.VIGILANT")}</option>    
+					</select>
+					</div>
+					<div style="display:flex; margin-top:5px; margin-bottom:5px">
+					<label style="min-width:15em" for="altdam">Damage:</label>
+					<input style="max-width:10em" id="altdam" name="altdam" type="text">
+					</div>
+					</form>
+					`,
+				buttons: {
+					yes: {
+						icon: "<i class='fas fa-check'></i>",
+						label: `Apply Damage`,
+						callback: async (html)=> {
+								await this.dealDamage(html.find("#vision-type")[0].value, html.find('#altdam')[0].value);
+							}
+					},
+					no: {
+						icon: "<i class='fas fa-times'></i>",
+						label: `Cancel Damage`
+					},
+				},
+				default: "yes",
+			}).render(true);
+		}
+
+		async dealDamage(type, damage)
+		{
+				for ( let token of canvas.tokens.controlled ) {
+						let calcDam = parseInt(damage) * -1;
+						if( isNaN(calcDam)) {
+								console.log("Can't understand damage["+damage+"] - is this a number?");
+								break;
+						}
+						let actor = token.actor;
+						if( actor.system.attributes[type] === undefined || actor.system.attributes[type] === null) {
+								console.log("This is not an attribute in Symbaroum");
+								break;
+						}
+						let tot = actor.system.attributes[type].temporaryMod + calcDam;
+						let modification = {        };
+						setProperty(modification, `system.attributes.${type}.temporaryMod`, tot);        
+						await actor.update(modification);
+						ui.notifications.info(`${actor.name} takes ${damage} damage to ${game.i18n.localize(actor.system.attributes[type].label)}.`);
+
+				}
+		}
     /**********************************************
      * Macro: 
      */
