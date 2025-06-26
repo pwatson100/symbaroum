c6f0f3bdb21a4be319716b569abae01d03cbca6d
Peter Norell <bithir@gmail.com>
2025-06-26 13:16:19 +0100

    Fix of #752, #526

--- script/common/macro.js
+++ script/common/macro.js
@@ -511,9 +511,47 @@ export class SymbaroumMacros {
 	}
 
     /**********************************************
-     * Macro: 
+     * Macro: Make a monster a PC, make a PC a monster
      */
+	async togglePlayerNPC()
+	{
+    let dialog_content = `  
+    <div class="form-group">
+        Type the exact name of the player/npc - ensure the Player/NPC has a unique name among all your actors.  <br/> A NPC will be made into a player. A player will be made into an NPC.<br/>
+      <label for="npctext">NPC name</label>
+      <input name="npctext" type="text">
+    </div>`;
+  
+    let x = new Dialog({
+      content : dialog_content,
+      buttons : 
+      {
+        Ok : { label : `Ok`, callback : async (html)=> await this.change2PC(html.find('[name=npctext]')[0].value.replace(/[\r|\n]/g, ""))},
+        Cancel : {label : `Cancel`}
+      }
+    });
+  
+    x.options.width = 200;
+    x.position.width = 200;
+  
+    x.render(true);
+	}
 
+	async change2PC(npcname)
+	{
+    let myActor = game.actors.getName(npcname);
+    console.log(myActor);
+    if( myActor === null) {
+        ui.notifications.error(`Could not find actor with name ${npcname}. Try again`);
+        return;
+    }
+    let update = { 
+        "type" : myActor.type === "player" ? "monster":"player",
+				"==system" : myActor.toObject().system
+    };
+    await myActor.update(update);
+    ui.notifications.info(`Actor with name ${npcname} is now a ${update.type}.`);
+	}
     /**********************************************
      * Macro: 
      */
