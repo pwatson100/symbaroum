17777151d9987337c147619c46e56ef362ac355b
Peter Norell <bithir@gmail.com>
2025-06-26 18:02:39 +0100

    Fix for #701 and Convert Macros to system #526

--- script/common/macro.js
+++ script/common/macro.js
@@ -715,8 +715,76 @@ export class SymbaroumMacros {
 		}
 
     /**********************************************
-     * Macro: 
+     * Macro: resetTemporaryCorruption
      */
+/** 
+ * Macro can be used by either selecting tokens on-screen or, if no tokens are selected, choosing which player characters (default all)
+ * 
+ */
+		async resetTemporaryCorruption()
+		{
+				let defaultCheck = "checked"; // set to unchecked
+				const actorslist = this.getPlayerList();
+				let allKeys = "";
+				actorslist.forEach(t => {
+						allKeys = allKeys.concat(`<div style="flex-basis: auto;flex-direction: row;display: flex;">
+										<div style="width:10em;min-width:10em;"><label for="${t.id}">${t.name}</label> </div>
+										<div><input id="${t.id}" type="checkbox" name="selection" value="${t.id}" ${defaultCheck}="${defaultCheck}"></div>
+								</div>`);
+				});
+
+				let dialog_content = `  
+				<div class="form-group">
+				<h2>Select player(s)</h2>
+				${allKeys}
+				<br />
+				</div>`;
+				let x = new Dialog({
+						title: "Reset Corruption",
+						content : dialog_content,
+						buttons : 
+						{
+								Ok :{ label : `Ok`, callback : async (html) => {             
+																								let tmp = html.find("input[name='selection']").get().filter(v => { if(v.checked) return true; }).map(e => { return e.value});                                            
+																								if(tmp.length == 0) {
+																										ui.notifications.error("Need a valid number of players");
+																										return;
+																								}
+																								this.resetCorruption(tmp);
+																						}
+										},
+								Cancel : {label : `Cancel`}
+						}
+				});
+				
+				x.options.width = 200;
+				x.position.width = 300;
+				
+				x.render(true);
+		}
+
+		async resetCorruption(actorids)
+		{
+				let actorNames = "";
+				let updates = actorids.map(a => {
+						let aexp = game.actors.get(a);
+				
+						actorNames = actorNames + "<li>" + aexp.name;
+
+						return {
+								_id: a,
+								"system.health.corruption.temporary": 0
+						};
+				});
+				
+				Actor.updateDocuments(updates);
+				let chatOptions = {
+						rollMode: game.settings.get('core', 'rollMode'),        
+						content: `<h2>Temporary corruption was washed away</h2> 
+												The following actors:<ul> ${actorNames}</ul> is now at zero temporary corruption`
+				};
+				ChatMessage.create(chatOptions);
+		}		
     /**********************************************
      * Macro: 
      */
